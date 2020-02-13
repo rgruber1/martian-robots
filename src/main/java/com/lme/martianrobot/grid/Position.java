5fb03634ac1330d421c2c345d8030442c9c8e850
Robert Gruber <gruber.robert@gmail.com>
2020-02-13 00:03:39 +0000

    added grid tests functionality

--- src/main/java/com/lme/martianrobot/grid/Position.java
+++ src/main/java/com/lme/martianrobot/grid/Position.java
@@ -2,12 +2,20 @@ package com.lme.martianrobot.grid;
 
 public class Position {
 
+    private final boolean isLost;
     private Coordinate coordinate;
     private Orientation orientation;
 
     public Position(Coordinate coordinate, Orientation orientation) {
         this.coordinate = coordinate;
         this.orientation = orientation;
+        this.isLost = false;
+    }
+
+    public Position(Coordinate coordinate, Orientation orientation, boolean isLost) {
+        this.coordinate = coordinate;
+        this.orientation = orientation;
+        this.isLost = isLost;
     }
 
     public Coordinate getCoordinate() {
@@ -18,4 +26,7 @@ public class Position {
         return orientation;
     }
 
+    public boolean isLost() {
+        return isLost;
+    }
 }
